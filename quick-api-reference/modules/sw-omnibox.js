92a9f3ce2ebbd341697fad9e3c27359a6e22263c
Amr-Guaily <amrguaily@gmail.com>
2024-06-01 22:22:16 +0300

    using omnibox to interact with extension

---
+++ quick-api-reference/modules/sw-omnibox.js
@@ -0,0 +1,22 @@
+// destructure the 'reason' property from the event object
+
+// onInstalled => this event will be fired when the extension is first installed, updated to a new version, or chrome is updated to a new version.
+// when a user installs the extension for the fisrt time, this event fires.
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+    if (reason === 'install') {
+        chrome.storage.local.set({
+            apiSuggestion: ['tabs', 'storage', 'scripting']
+        });
+    }
+});
+
+const URL_CHROME_EXTENSIONS_DOC = 'https://developer.chrome.com/docs/extensions/reference/';
+
+// Display the suggestions after user starts typing => When the user enters the omnibox keyword (api), chrome will display a list of suggestions based on the keywords in storage.
+// addListener => this method adds a listener to handle the input change event.
+// suggest => a callback function used to handle suggestions.
+chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
+    await chrome.omnibox.setDefaultSuggestion({
+        description: 'Enter a chrome API or choose from past searches'
+    });
+});
---
+++ quick-api-reference/service-worker.js
@@ -0,0 +1,3 @@
+import './modules/sw-omnibox.js';
+import './modules/sw-tips.js';
+
