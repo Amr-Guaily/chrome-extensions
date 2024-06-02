02d3fc95963f8ef7f048be1a92ee6741b64e50bb
Amr-Guaily <amrguaily@gmail.com>
2024-06-02 16:15:29 +0300

    Most recent search used as an omnibox suggestion

--- quick-api-reference/modules/sw-omnibox.js
+++ quick-api-reference/modules/sw-omnibox.js
@@ -11,6 +11,7 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
 });
 
 const URL_CHROME_EXTENSIONS_DOC = 'https://developer.chrome.com/docs/extensions/reference/';
+const NUMBER_OF_PREVIOUS_SEARCHES = 4;
 
 // Display the suggestions after user starts typing => When the user enters the omnibox keyword (api), chrome will display a list of suggestions based on the keywords in storage.
 // addListener => this method adds a listener to handle the input change event.
@@ -27,3 +28,20 @@ chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
 
     suggest(suggestions);
 });
+
+// After the user selects a suggestion => open the corresponding Chrome API referance page.
+chrome.omnibox.OnInputEntered.addListener((input) => {
+    chrome.tabs.create({ url: URL_CHROME_EXTENSIONS_DOC + input });
+
+    // save the latest key word
+    updateHistory(input);
+});
+
+// This way the most recent search can be used later as an omnibox suggestion.
+async function updateHistory(input) {
+    const { apiSuggestions } = await chrome.storage.local.get('apiSuggestions');
+    apiSuggestions.unshift(input);
+    apiSuggestions.splice(NUMBER_OF_PREVIOUS_SEARCHES);
+
+    await chrome.storage.local.set({ apiSuggestions });
+}
