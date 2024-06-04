b80c57a7ebae1fdb3a05e2a2fdc25977d8285131
Amr-Guaily <amrguaily@gmail.com>
2024-06-04 11:25:36 +0300

    communicate with content-script

--- quick-api-reference/service-worker.js
+++ quick-api-reference/service-worker.js
@@ -1,3 +1,10 @@
 import './modules/sw-omnibox.js';
 import './modules/sw-tips.js';
 
+// send tip to content script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.greeting === 'tip') {
+        chrome.storage.local.get('tip').then(sendResponse);
+        return true;
+    }
+}); 
